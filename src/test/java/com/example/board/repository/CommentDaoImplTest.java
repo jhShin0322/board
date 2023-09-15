464b5701864806c3c48a22b96048cfbbd048825a
jhShin1557 <sinmagic1@naver.com>
2023-09-15 15:32:31 +0900

    댓글 구현 작업중

--- src/test/java/com/example/board/repository/CommentDaoImplTest.java
+++ src/test/java/com/example/board/repository/CommentDaoImplTest.java
@@ -1,6 +1,5 @@
 package com.example.board.repository;
 
-import com.example.board.domain.CommentDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/com/example/board/service/CommentServiceImplTest.java
+++ src/test/java/com/example/board/service/CommentServiceImplTest.java
@@ -1,9 +1,7 @@
 package com.example.board.service;
 
 import com.example.board.domain.BoardDto;
-import com.example.board.domain.CommentDto;
 import com.example.board.repository.BoardDao;
-import com.example.board.repository.CommentDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
